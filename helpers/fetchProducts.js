bf426c92a327f84f2d71a8bf36a7c316339956f2
raphaelbaere <raphaelbaere@id.uff.br>
2022-10-11 15:43:02 -0300

    Requisito 1 concluído

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,25 @@
-const fetchProducts = () => {
-  // seu código aqui
+const createUrl = (name) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${name}`;
+  return url;
+};
+
+const validUrl = (name) => {
+    if (name) {
+      return true;
+  }
+  throw new Error('You must provide an url');
+};
+
+const fetchProducts = async (name) => {
+  try {
+    validUrl(name);
+    const url = createUrl(name);
+    const response = await fetch(url);
+    const objetoRetornado = await response.json(); 
+    return objetoRetornado;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,28 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  const parameter = 'computador';
+  test('é uma função', () => {
+    expect.assertions(1);
+    const expected = 'function';
+    expect(typeof fetchProducts).toBe(expected);
+    })
+    test('se fetch é chamado', async () => {
+      expect.assertions(1);
+      await fetchProducts(parameter);
+      expect(fetch).toHaveBeenCalledTimes(1);
+    })
+    test('utiliza o endpoint correto', async () => {
+      expect.assertions(1);
+      await fetchProducts(parameter);
+      expect(fetch).toHaveBeenCalledWith(`https://api.mercadolibre.com/sites/MLB/search?q=${parameter}`);
+    })
+    test('se retorna o objeto correto', async () => {
+      expect.assertions(1);
+      expect(await fetchProducts(parameter)).toEqual(computadorSearch);
+    })
+    test('se retorna um erro ao chamar sem parâmetros', async () => {
+      expect.assertions(1);
+      await expect(fetchProducts()).resolves.toThrow(new Error('You must provide an url'))
+    })
 });
