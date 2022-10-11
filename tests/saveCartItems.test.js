48f9e528d91feae4e5ea5cdd23b8acfd2a335794
raphaelbaere <raphaelbaere@id.uff.br>
2022-10-11 18:17:50 -0300

    Requisito 6 concluído

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,9 +1,15 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const saveCartItems = require('../helpers/saveCartItems');
-
+const cartItem = 'MLB1615760527';
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('se passar cartItem como parâmetro, chama o método localStorage.setItem', () => {
+    saveCartItems(cartItem);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  })
+  test('se passar cartItem como parâmetro, executa localStorage.setItem com dois parãmetros corretos.', () => {
+    saveCartItems(cartItem);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', cartItem);
+  })
 });
